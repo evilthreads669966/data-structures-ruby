c9ac8db24d8bc24e35e0ac4d03865fd79fa16f4b
chrisbasinger <evilthreads669966@gmail.com>
2025-01-01 10:12:07 -0600

    add ArrayStack

---
+++ rspec/array_stack_test.rb
@@ -0,0 +1,78 @@
+require 'rspec'
+require_relative '../stacks/array_stack'
+
+describe ArrayStack do
+  before do
+    @stack = ArrayStack.new
+    @stack.push(1)
+    @stack.push(2)
+    @stack.push(3)
+    @stack.push(4)
+    @stack.push(5)
+  end
+
+  describe 'clear' do
+    it "clear should remove all values in the stack's array" do
+      @stack.clear
+      expect(@stack.length).to eq(0)
+    end
+  end
+
+  describe "empty?" do
+    it "empty? should return true if there are no values in the stack" do
+      expect(@stack.empty?).to be false
+      @stack.clear
+      expect(@stack.empty?).to be true
+    end
+  end
+
+  describe "push" do
+    it "push should add a value to the top of the stack" do
+      @stack.push(6)
+      expect(@stack.pop).to eq(6)
+    end
+  end
+
+  describe "pop" do
+    it "pop should remove the value at the top of the stack and return it" do
+      expect(@stack.pop).to eq(5)
+      @stack.clear
+      expect{ @stack.pop }.to raise_error(NoSuchElementError)
+    end
+  end
+
+  describe "peek" do
+    it "peek should return the value at the top of the stack" do
+      expect(@stack.peek).to eq(5)
+      @stack.clear
+      expect{ @stack.peek }.to raise_error(NoSuchElementError)
+    end
+  end
+
+  describe "length" do
+    it "length should return the number of values in the stack" do
+      expect(@stack.length).to eq(5)
+      @stack.push(6)
+      expect(@stack.length).to eq(6)
+      @stack.pop
+      expect(@stack.length).to eq(5)
+    end
+  end
+
+  describe "selection_sort" do
+    it "selection sort should sort the values in the stack in either ascending or descending order" do
+      @stack.clear
+      @stack.push(3)
+      @stack.push(5)
+      @stack.push(1)
+      @stack.push(4)
+      @stack.push(2)
+
+      @stack.selection_sort(SortingType::ASCENDING)
+      puts @stack
+      for i in 5..1
+        expect(@stack.pop).to eq(i)
+      end
+    end
+  end
+end
---
+++ stacks/array_stack.rb
@@ -0,0 +1,83 @@
+require_relative '../exceptions'
+require_relative '../sorting_type'
+
+class ArrayStack
+  private
+  attr :array, :top
+
+  public
+  attr_reader :length
+  def initialize
+    @array = []
+    @top = -1
+    @length = 0
+  end
+
+  def empty?
+    @top == -1
+  end
+
+  def push(value)
+    @length += 1
+
+    @top += 1
+    @array[@top] = value
+  end
+
+  def pop
+    raise NoSuchElementError if empty?
+
+    @length -= 1
+
+    value = @array[@top]
+    @top -= 1
+
+    value
+  end
+
+  def peek
+    raise NoSuchElementError if empty?
+
+    @array[@top]
+  end
+
+  def clear
+    @array = []
+    @top = -1
+    @length = 0
+  end
+
+  def selection_sort(sorting_type = SortingType::ASCENDING)
+    return if empty?
+
+    for i in 0..top
+      for j in i + 1..top
+        if sorting_type == SortingType::ASCENDING
+          if @array[i] > @array[j]
+            temp = array[i]
+            @array[i] = @array[j]
+            @array[j] = temp
+          end
+        else
+          if @array[i] < @array[j]
+            temp = @array[i]
+            @array[i] = @array[j]
+            @array[j] = temp
+          end
+        end
+      end
+    end
+  end
+
+  def to_s
+    return "" if empty?
+
+    string = "["
+
+    @array.each do |value|
+      string << value.to_s << " "
+    end
+
+    string << "]"
+  end
+end
